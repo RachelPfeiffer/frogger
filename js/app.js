5684dc5d988262dd807e5233c230ec334cf6a0c8
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-10-22 23:52:50 +0300

    redid all game functionality

--- js/app.js
+++ js/app.js
@@ -1,38 +1,37 @@
 // Enemies our player must avoid
-var Enemy = function(x,y,speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function() {
+  const randomX = 10;
+  const possibleYs = [60, 130, 200];
+  const randomYpicker = Math.floor((Math.random() * 3)) ;
+  const randomY = possibleYs[randomYpicker];
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    this.x = randomX;
+    this.y = randomY;
+    this.speed = Math.random()*150 + 100;
 };
-const randomX = Math.floor(Math.random() * 5);
-const yOptions = [210, 130, 50];
-const randomY = function () {
-  const randomLine = Math.floor((Math.random() * 3));
-  return yOptions[randomLine];
-};
-const speedOptions = [50, 75, 75, 100, 150, 200]
-const randomSpeed = function () {
-  const pickSpeed = Math.floor((Math.random() * 5));
-  return speedOptions[pickSpeed];
-};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x = this.x + this.speed * dt;
-  // You should multiply any movement by the dt parameter
+  const possibleYs = [60, 130, 200];
+  const randomYpicker = Math.floor((Math.random() * 3)) ;
+  const randomY = possibleYs[randomYpicker];
+  this.x = this.x += this.speed * dt;
+  if (this.x > 500) {
+    this.x = -100;
+    this.speed = (Math.random()*150 + 100);
+    this.y = randomY;
+  }
+  if (Math.abs(player.x - this.x) < 70 && Math.abs(player.y - this.y) < 15) {
+  player.x = 200;
+  player.y = 370;
+}
+    // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x > 505) {
-      this.x = -50;
-      this.y = randomY();
-      this.speed = randomSpeed();
-    };
 };
 
 // Draw the enemy on the screen, required method for game
@@ -41,74 +40,60 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 var Player = function() {
   this.sprite = 'images/char-boy.png';
   this.x = 200;
   this.y = 370;
-}
-// This class requires an update(), render() and
-// a handleInput() method.
-Player.prototype.update = function() {
-  if (this.y<-30) {
-    this.y = -30;
-  } else if (this.y > 370) {
-    this.y = 370;
-  } else if (this.x < 0) {
-    this.x = 0;
-  } else if (this.x > 400) {
-    this.x = 400;
-  } else if ((Math.abs(this.x - enemyOne.x) < 55) && this.y === enemyOne.x || (Math.abs(this.x - enemyTwo.x) < 55 && this.y === enemyTwo.y) || (Math.abs(this.x - enemyThree.x < 55) && enemyThree.y === enemyTwo.y)) {
-    this.x = 200;
-    this.y = 370;
-  } else if (this.y = -30) {
-    const youWin = document.createElement("div");
-    youWin.innerText = 'You win!'
-    const window = document.querySelector("body");
-    window.appendChild(youWin);
-    document.removeEventListener('keyup', function(e) {
-        var allowedKeys = {
-            37: 'left',
-            38: 'up',
-            39: 'right',
-            40: 'down'
-        };
-
-        player.handleInput(allowedKeys[e.keyCode]);
-    });
-
-  }
 };
 
-Player.prototype.handleInput = function(key) {
-  if (key === 'left') {
+Player.prototype.update = function() {
+  //if player reaches top, reset
+  if (this.y < -20) {
+    this.y = -20;
+    setTimeout(function() {
+      confirm('You Win!');
+    },100);
+  };
+
+  if (this.x < 0) {
+    this.x = 0;
+  };
+
+  if (this.x > 400) {
+    this.x = 400;
+  };
+
+  if (this.y > 370) {
+    this.y = 370;
+  };
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keydown) {
+  if (keydown === 'left') {
     this.x -= 100;
-  } else if (key === 'up') {
-    this.y -= 80;
-  } else if (key === 'right') {
+  } else if (keydown === 'right') {
     this.x += 100;
-  } else if (key === 'down'){
+  } else if (keydown === 'up') {
+    this.y -= 80;
+  } else if (keydown === 'down') {
     this.y += 80;
   }
 }
-// Draw the enemy on the screen, required method for game
-  Player.prototype.render = function() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const enemyOne = new Enemy(randomX, randomY(), randomSpeed());
-const enemyTwo = new Enemy(randomX, randomY(), randomSpeed());
-const enemyThree = new Enemy(randomX, randomY(), randomSpeed());
-
-const allEnemies = [enemyOne,enemyTwo,enemyThree];
+const EnemyOne = new Enemy();
+const EnemyTwo = new Enemy();
+const EnemyThree = new Enemy();
+const EnemyFour = new Enemy();
+const allEnemies = [EnemyOne, EnemyTwo, EnemyThree, EnemyFour];
 const player = new Player();
-allEnemies.forEach(function randomize(enemy){
-  enemy.x = randomX;
-  enemy.y = randomY();
-  enemy.speed = randomSpeed();
-});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
